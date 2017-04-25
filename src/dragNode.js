bdf28f7bde4af23c0e3d41e1971b0b7940761a8d
Bogdan Chadkin <trysound@yandex.ru>
2017-04-25 03:31:48 +0300

    Code tweaks

--- src/dragNode.js
+++ src/dragNode.js
@@ -2,25 +2,29 @@ const wrap = (min, value, max) => Math.min(Math.max(min, value), max);
 
 const resizeNode = (node, params, action, resize) => {
     const { cellWidth, cellHeight, minWidth = 1, minHeight = 1, maxWidth = Infinity, maxHeight = Infinity } = params;
-    const nodeX = node.x * cellWidth;
-    const nodeWidth = node.width * cellWidth;
-    const nodeY = node.y * cellHeight;
-    const nodeHeight = node.height * cellHeight;
 
     const minX = maxWidth === Infinity ? 0 : node.x + node.width - maxWidth;
     const minY = maxHeight === Infinity ? 0 : node.y + node.height - maxWidth;
     const maxX = node.x + node.width - minWidth;
     const maxY = node.y + node.height - minHeight;
 
+    const dirX = (resize.left ? -1 : resize.right ? 1 : 0);
+    const dirY = (resize.top ? -1 : resize.bottom ? 1 : 0);
+
+    const elementX = node.x * cellWidth;
+    const elementWidth = node.width * cellWidth;
+    const elementY = node.y * cellHeight;
+    const elementHeight = node.height * cellHeight;
+
     const endX
         = resize.left
-        ? Math.max(action.startX - nodeX, action.endX)
+        ? Math.max(action.startX - elementX, action.endX)
         : resize.right
         ? action.endX
         : action.startX;
     const endY
         = resize.top
-        ? Math.max(action.startY - nodeY, action.endY)
+        ? Math.max(action.startY - elementY, action.endY)
         : resize.bottom
         ? action.endY
         : action.startY;
@@ -29,27 +33,20 @@ const resizeNode = (node, params, action, resize) => {
     const elementDy = endY - action.startY;
     const nodeDx = Math.floor(endX / cellWidth) - Math.floor(action.startX / cellWidth);
     const nodeDy = Math.floor(endY / cellHeight) - Math.floor(action.startY / cellHeight);
+
     return {
         type: 'resize',
         element: {
-            x: wrap(minX * cellWidth, nodeX + (resize.left ? elementDx : 0), maxX * cellWidth),
-            y: wrap(minY * cellHeight, nodeY + (resize.top ? elementDy : 0), maxY * cellHeight),
-            width: wrap(
-                minWidth * cellWidth,
-                nodeWidth + (resize.left ? -1 : resize.right ? 1 : 0) * elementDx,
-                maxWidth * cellWidth
-            ),
-            height: wrap(
-                minHeight * cellHeight,
-                nodeHeight + (resize.top ? -1 : resize.bottom ? 1 : 0) * elementDy,
-                maxHeight * cellHeight
-            )
+            x: wrap(minX * cellWidth, elementX + (resize.left ? elementDx : 0), maxX * cellWidth),
+            y: wrap(minY * cellHeight, elementY + (resize.top ? elementDy : 0), maxY * cellHeight),
+            width: wrap(minWidth * cellWidth, elementWidth + dirX * elementDx, maxWidth * cellWidth),
+            height: wrap(minHeight * cellHeight, elementHeight + dirY * elementDy, maxHeight * cellHeight)
         },
         node: Object.assign({}, node, {
             x: wrap(minX, node.x + (resize.left ? nodeDx : 0), maxX),
             y: wrap(minY, node.y + (resize.top ? nodeDy : 0), maxY),
-            width: wrap(minWidth, node.width + (resize.left ? -1 : resize.right ? 1 : 0) * nodeDx, maxWidth),
-            height: wrap(minHeight, node.height + (resize.top ? -1 : resize.bottom ? 1 : 0) * nodeDy, maxHeight)
+            width: wrap(minWidth, node.width + dirX * nodeDx, maxWidth),
+            height: wrap(minHeight, node.height + dirY * nodeDy, maxHeight)
         })
     };
 };
